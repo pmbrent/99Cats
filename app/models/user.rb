258bc1ccfbc8ff6a62ea325f174909e5f3a1a0a2
App Academy Student <appacademy@Apps-Mac-mini-29.local>
2015-10-21 12:16:07 -0400

    Implenent User model authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,33 @@
 class User < ActiveRecord::Base
+
+  validates :user_name, :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true}
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(user_name: user_name)
+    user.is_password?(password) ? user : nil
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest) ==
+      BCrypt::Password.create(password)
+  end
+
+
 end
